6226488f635a7d3b5f9cb21ddd0e48fdadd2929a
igor06brw <f56JFS20ff@protonmail.com>
2021-06-29 19:21:15 +0200

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,28 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <nav>
-                <ul>
-                    {/* AFTER ADD ROUTER-DOM, CHANGE TO { Link } */}
-                    <li>
-                        <a href="">Create Task</a>
-                        {/* // Assign icon "add" after implement font-awesome */}
-                    </li>
-                    <li>
-                        <a href="">Unfinished Tasks</a>
-                        {/* // Assign icon "time" after implement font-awesome */}
-                    </li>
-                    <li>
-                        <a href="">Finished Tasks</a>
-                        {/* // Assign icon "finish" after implement font-awesome */}
-                    </li>
-                </ul>
-            </nav>
+            <div>
+                <div>
+                    <h2>Todolist</h2>
+                </div>
+                <nav>
+                    <ul>
+                        {/* AFTER ADD ROUTER-DOM, CHANGE TO { Link } */}
+                        <li>
+                            <a href="">Create Task</a>
+                            {/* // Assign icon "add" after implement font-awesome */}
+                        </li>
+                        <li>
+                            <a href="">Unfinished Tasks</a>
+                            {/* // Assign icon "time" after implement font-awesome */}
+                        </li>
+                        <li>
+                            <a href="">Finished Tasks</a>
+                            {/* // Assign icon "finish" after implement font-awesome */}
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }

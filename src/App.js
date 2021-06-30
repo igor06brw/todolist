7d53ae4c1cc3dc16f4a217ce8d971b6fb4c1636f
igor06brw <f56JFS20ff@protonmail.com>
2021-06-30 17:47:50 +0200

    init themes & themeprovider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import styled, { ThemeProvider } from 'styled-components';
+import { example } from './theme/themes';
+
+const AppWrapper = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`
 
 function App() {
+
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <ThemeProvider theme={example}>
+      <AppWrapper className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </AppWrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/themes.js
@@ -0,0 +1,5 @@
+const example = {
+    bgColor: "#282c34",
+}
+
+export { example }

09b284ee64a15b20ee46c09fa2bb922fe452fd68
igor06brw <f56JFS20ff@protonmail.com>
2021-07-12 14:54:50 +0200

    create tasks array of objects

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+const tasks = [
+    {
+        _id: 0,
+        title: "Task 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        _id: 1,
+        title: "Task 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        _id: 2,
+        title: "Task 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+]

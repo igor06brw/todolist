47580ae38f37fa1165a7dcff3a94a38db6f428ec
igor06brw <f56JFS20ff@protonmail.com>
2021-07-17 12:47:26 +0200

    display CreateTaskScreen

--- src/screens/CreateTaskScreen.js
+++ src/screens/CreateTaskScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 
 const ModalContainer = styled.div`
-    display:none;
+    display:flex;
     background: rgba(0,0,0,0.8);
     width: 100vw;
     height: 100vh;

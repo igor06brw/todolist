248bbe690c54f6aa54e95252a01b7d39814e9e1a
Igor <f56JFS20ff@protonmail.com>
2021-07-21 10:18:42 +0200

    display component behind createtask

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from "react-router-dom";
 
 import CreateTaskScreen from '../screens/CreateTaskScreen'
+import DisplayTaskScreen from '../screens/DisplayTaskScreen';
 
 const Main = ({ children }) => {
-    console.log(useHistory)
+    const history = useHistory();
+    const [pathname, setPathname] = useState('/')
+    const [previousComponent, setPreviousComponent] = useState(<DisplayTaskScreen />)
+    const [previousPathname, setPreviousPathname] = useState('/')
+    const [currentComponent, setCurrentComponent] = useState()
+   
+    history.listen((location) => {
+        setPreviousPathname((pathname === '/createtask') ? ('/') : (pathname));
+        setPathname(location.pathname);
+        console.log(pathname === '/createtask' && previousPathname === '/')
+    });
+    
+    console.log(previousPathname, pathname)
 
     return (
         <div>
-            {children}
+            {
+                (pathname === '/createtask' && previousPathname === '/') ? (
+                    <>
+                        {
+                            children
+                        }
+                        {
+                            <DisplayTaskScreen />
+                        }
+                    </>
+                ) : (pathname === '/createtask' && previousPathname === '/finishtask') ? (
+                    <>
+                        {
+                            children
+                        }
+                        {
+                            <>XD</>
+                        }
+                    </>
+                ) : (children)
+            }   
         </div>
     )
 }

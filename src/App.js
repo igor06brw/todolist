560903a516d3d2726de26d7b095a8b132bbeac9f
Igor <f56JFS20ff@protonmail.com>
2021-07-22 12:35:41 +0200

    add finished task route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import DisplayTaskScreen from './screens/DisplayTaskScreen';
 import CreateTaskScreen from './screens/CreateTaskScreen';
+import FinishedTaskScreen from './screens/FinishTaskScreen';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Main>
         <Route path="/" exact component={DisplayTaskScreen} />
         <Route path="/createtask" component={CreateTaskScreen} />
+        <Route path="/finishtask" component={FinishedTaskScreen} />
       </Main>
       <Footer />
     </Router>

64cdfc914b8c8744a695b9ce041977ab95da7f11
Igor <f56JFS20ff@protonmail.com>
2021-07-22 12:37:19 +0200

    add finished example object of finish tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,4 @@
-const tasks = [
+export const tasks = [
     {
         _id: 0,
         title: "Task 1",
@@ -16,4 +16,22 @@ const tasks = [
     }
 ]
 
-export default tasks;
+export const finishedTasks = [
+    {
+        _id: 5,
+        title: "Task 6",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        _id: 6,
+        title: "Task 7",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        _id: 7,
+        title: "Task 8",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+]
+
+export default {}

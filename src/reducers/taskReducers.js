6268a73897e96708d7e283851026e9be66d5b36f
Igor <f56JFS20ff@protonmail.com>
2021-07-23 13:21:28 +0200

    #TEST create task reducer

---
+++ src/reducers/taskReducers.js
@@ -0,0 +1,10 @@
+import { TASK_ADD_ITEM } from "../constants/taskConstants"
+
+
+
+export const taskReducer = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}

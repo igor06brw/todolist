d8abdd881d5ffe679c47cf42e9d928768d291e15
Igor <f56JFS20ff@protonmail.com>
2021-07-28 17:29:26 +0200

    add task create reducer

--- src/reducers/taskReducers.js
+++ src/reducers/taskReducers.js
@@ -1,4 +1,4 @@
-import { TASK_LIST_ERROR, TASK_LIST_REQUEST, TASK_LIST_SUCCESS } from "../constants/taskConstants"
+import { TASK_CREATE_ERROR, TASK_CREATE_REQUEST, TASK_CREATE_SUCCESS, TASK_LIST_ERROR, TASK_LIST_REQUEST, TASK_LIST_SUCCESS } from "../constants/taskConstants"
 
 export const taskReducer = (state = { tasks: []}, action) => {
     switch (action.type) {
@@ -16,10 +16,23 @@ export const taskReducer = (state = { tasks: []}, action) => {
 export const taskDeleteReducer = (state = {}, action) => {
     switch (action.type) {
         case TASK_LIST_REQUEST:
-            return { }
+            return {}
         case TASK_LIST_SUCCESS:
-            return { success: true }
+            return {success: true }
         case TASK_LIST_ERROR:
+            return { error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const taskCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case TASK_CREATE_REQUEST:
+            return { }
+        case TASK_CREATE_SUCCESS:
+            return { success: true, data: action.payload }
+        case TASK_CREATE_ERROR:
             return { error: action.payload }
         default:
             return state

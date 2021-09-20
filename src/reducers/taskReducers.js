fe462378bdad2707c23d7767cb0ade8fc84a0891
Igor <f56JFS20ff@protonmail.com>
2021-09-20 03:45:23 +0200

    complete reducer of tasks

--- src/reducers/taskReducers.js
+++ src/reducers/taskReducers.js
@@ -1,16 +1,44 @@
 import { FETCH_TASKS_REQUEST, FETCH_TASKS_SUCCESS, FETCH_TASKS_FAILURE, CREATE_TASK, EDIT_TASK, DELETE_TASK} from "../constants/taskConstants"
 
-export const tasks = (state = [], action) => {
+const initialState = {
+    loading: false,
+    tasks: [],
+    error: ''
+}
+
+export const taskReducer = (state = initialState, action) => {
+    
+    
+
     switch (action.type) {
         case FETCH_TASKS_REQUEST:
-        
+            return {
+                ...state,
+                loading: true
+            }
         case FETCH_TASKS_SUCCESS:
-
+            return {
+                ...state,
+                loading: false,
+                tasks: action.payload,
+            }
         case FETCH_TASKS_FAILURE:
-
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
         case CREATE_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.concat(action.payload)
+            }
 
         case DELETE_TASK:
+            return {
+                ...state,
+
+            }
 
         case EDIT_TASK:
 

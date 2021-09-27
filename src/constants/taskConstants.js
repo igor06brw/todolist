294cc5fd9588df3dcd508fd560d2209591b3c838
Igor <f56JFS20ff@protonmail.com>
2021-09-27 22:09:40 +0200

    const of completeTask for redux

--- src/constants/taskConstants.js
+++ src/constants/taskConstants.js
@@ -4,3 +4,4 @@ export const FETCH_TASKS_FAILURE = 'FETCH_TASKS_FAILURE';
 export const CREATE_TASK = 'CREATE_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
 export const EDIT_TASK = 'EDIT_TASK';
+export const COMPLETE_TASK = 'COMPLETE_TASK';
